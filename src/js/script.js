97217831ac28723184ae06fa41c3269bccdbb226
Arthur Fiorese <arthur.herrada@gmail.com>
2026-04-01 21:55:02 -0300

    feat:Adição de JavaScript

---
+++ src/js/script.js
@@ -0,0 +1,33 @@
+// Alert de introdução ao site.
+alert("Bem vindo ao site da Vinharia Agnello! Explore nossos vinhos e aproveite a experiência única que oferecemos.");
+
+// Prompt para o usuário inserir seu nome para realizar o cadastro.
+let nomedeusuario = prompt("Por favor, insira seu nome para uma experiência personalizada:");
+
+// Exibindo uma mensagem de boas-vindas personalizada no console e um alerta de confirmação de cadastro.
+console.log(`Bem-vindo, ${nomedeusuario}! Estamos felizes em tê-lo conosco. Explore nossa seleção de vinhos e aproveite a experiência única que oferecemos. Estamos à disposição em caso de dúvidas!`);
+
+alert("Cadastro realizado com sucesso! Veja os detalhes no console!");
+
+
+// Prompt para o usuário escolher um vinho e obter detalhes sobre ele.
+let nomedovinho = prompt("Qual vinho você gostaria de conhecer melhor? (Digite o nome do vinho)");
+
+// Prompt para o usuário escolher o tipo de vinho.
+let tipodovinho = prompt("Qual tipo de vinho você prefere? (Tinto, Branco, Rosé)");
+
+// Prompt para o usuário escolher a safra do vinho.
+let safradovinho = prompt("Qual safra do vinho você gostaria de conhecer? (Digite o ano da safra)");
+
+// Exemplo de quantidade em estoque para o vinho selecionado.
+let quantidade_estoque = 10;
+
+// Exibindo os detalhes do vinho selecionado no console e um alerta para o usuário conferir as informações.
+console.log("Veja os detalhes do vinho selecionado:");
+console.log(`Nome do vinho: ${nomedovinho}.`);
+console.log(`Tipo de vinho: ${tipodovinho}.`);
+console.log(`Safra do vinho: ${safradovinho}.`);
+console.log(`Quantidade em estoque: ${quantidade_estoque}.`);
+alert("Confira os detalhes do vinho selecionado no console!");
+
+
